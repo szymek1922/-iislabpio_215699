16591d7419d76afef3ab5fa828a58010038c9f96
szymek1922 <44381046+szymek1922@users.noreply.github.com>
2020-01-28 10:28:48 +0100

    Update test.java

--- test.java
+++ test.java
@@ -1,3 +1,15 @@
+docker rm -vf $(docker ps -a -q)
+
+docker rmi -f $(docker images -a -q)
+
+
+docker pull wordpress
+docker pull mysql:5.7
+
+docker run --name mysql01 -e MYSQL_ROOT_PASSWORD=Password1234 -d mysql:5.7
+
+docker run --name wordpress01 --link mysql01 -p 8080:80 -e WORDPRESS_DB_HOST=mysql01:3306 -e WORDPRESS_DB_USER=root -e WORDPRESS_DB_PASSWORD=Password1234 -e WORDPRESS_DB_NAME=wordpress -e WORDPRESS_TABLE_PREFIX=wp_ -d wordpress
+
 // incorrect
 class anonymousUser {
 }
